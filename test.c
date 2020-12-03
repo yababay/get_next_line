8c1810630ef699e31b349adc3c7839e940759ff6
mabel <js@yababay.ru>
2020-12-03 17:41:33 +0300

    hello-world-as-a2-dim-array

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/uio.h>
 #include <sys/types.h>
@@ -6,20 +7,57 @@
 
 int main(int argc, char **argv)
 {
-    /*
 	int fd;
+    char *buff;
+    char **text;
+    size_t sz = 80;
+
+    buff = (char * ) malloc(sizeof(char)  * (sz + 1));
+    text = (char **) malloc(sizeof(char*) * (2));
+
+    const char *hello = "Hello, ";
+    const char *world = "World!" ;
+
+    char *mem1 = (char*) malloc(10);
+    char *mem2 = (char*) malloc(10);
+
+    char *m1 = mem1;
+    char *m2 = mem2;
+
+    *text = m1;
+    text++;
+    *text = m2;
+
+    while(*hello) *mem1++ = *hello++;
+    while(*world) *mem2++ = *world++;
+
+    *mem1 = '\0';
+    *mem2 = '\0';
+
+    printf("%s%s\n", *--text, *text);
+
+    /*
     if(argc > 2) return 1;
 
     fd = open(argv[1], O_RDONLY);
-    printf("%ld\n", fd);
-    //close(fd);
-*/
-    while ((ret = get_next_line(0, &buff)) > 0)
-        printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
-    if (ret == -1)
-        printf("-----------\nError\n");
-    else if (ret == 0)
-        printf("-----------\nEnd of stdin\n");
+
+    read(fd, buff, sz);
+
+    buff[sz] = '\0';
+
+    while(*buff){
+        printf("%c\n", *buff);
+        if(*buff == '\n'){
+            *buff = '\0';
+            *text = buff;
+            break;
+        }
+        buff++;
+    }
+
+    printf("%s\n", &(text[0]));
+
     close(fd);
+    */
     return 0;
 }
